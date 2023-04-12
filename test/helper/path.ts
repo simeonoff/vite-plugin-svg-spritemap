62c6e88e7ef0d5aa7c564e3c33f64f00dc9f85a6
Simeon Simeonoff <sim.simeonoff@gmail.com>
2023-04-12 17:48:14 +0300

    refactor(sass): update template

--- test/helper/path.ts
+++ test/helper/path.ts
@@ -1,5 +1,7 @@
-import { resolve } from 'path'
+import { fileURLToPath } from 'url';
+import path, { resolve } from 'path'
 import { normalizePath } from 'vite'
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export const getPath = (...pathSegments: string[]) => {
   return normalizePath(resolve(__dirname, './../', ...pathSegments))
